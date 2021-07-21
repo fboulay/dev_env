246f4349563f7ff67de987d894790899bed4240d
Florian Boulay <github@boulay.eu>
2021-07-21 11:06:01 +0200

    Update scripts to control pulse audio

--- define_sound_devices.sh
+++ define_sound_devices.sh
@@ -1,18 +1,53 @@
 #!/bin/bash
 
-if [[ "$#" -lt 2 || "$#" -gt 3 ]]; then
-    echo "Usage: $0 <input_device> <output_device> <output_port>" >&2
-    echo "  input_device: Source from pulse audio" >&2
-    echo "  output_device: Sink from pulse audio" >&2
-    echo "  output_port (optional): Sink port from pulse audio" >&2
+POSITIONAL=()
+while [[ $# -gt 0 ]]; do
+  key="$1"
+
+  case $key in
+    -i|--input-device)
+      inputDevice="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -o|--output-device)
+      outputDevice="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -ip|--input-port)
+      inputPort="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -op|--output-port)
+      outputPort="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    *)    # unknown option
+      POSITIONAL+=("$key") # save it in an array for later
+      shift # past argument
+      ;;
+  esac
+done
+
+if [[ -z "${inputDevice}" || -z "${outputDevice}" || "${#POSITIONAL[@]}" -gt 0 ]]; then
+    if [[ "${#POSITIONAL[@]}" -gt 0 ]]; then
+        echo "Unknown options ${POSITIONAL[@]}" >&2
+    fi
+
+    echo "Usage: $0 [option...]" >&2
+    echo >&2
+    echo "   -i,  --input-device          Source from pulse audio" >&2
+    echo "   -o,  --output-device         Sink from pulse audio" >&2
+    echo "   -ip, --input-port            Source port from pulse audio" >&2
+    echo "   -op, --output-port           Sink port from pulse audio" >&2
     exit 1
 fi
 
-source="$1"
-sink="$2"
-port="$3"
-
+# compute script directory
 scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-source ${scriptDir}/pulse_audio_source.sh ${source}
-source ${scriptDir}/pulse_audio_sink.sh ${sink} ${port}
+source ${scriptDir}/pulse_audio_source.sh ${inputDevice} ${inputPort}
+source ${scriptDir}/pulse_audio_sink.sh ${outputDevice} ${outputPort}
--- pulse_audio_source.sh
+++ pulse_audio_source.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-    echo "Usage: $0 <sinkId/sinkName>" >&2
+if [[ "$#" -lt 1 || "$#" -gt 2 ]]; then
+    echo "Usage: $0 <source_id/source_name> <source_port>" >&2
+    echo "  source_id/source_name: Source id or name" >&2
+    echo "  source_port (optional): Source port" >&2
     echo "Valid sources:" >&2
     pactl list short sources >&2
     exit 1
 fi
 
 newSource="$1"
+newPort="$2"
 
 pacmd set-default-source ${newSource}
 
@@ -16,3 +19,8 @@ pactl list short source-outputs|while read stream; do
     echo "moving input stream $streamId"
     pactl move-source-output "$streamId" "$newSource"
 done
+
+if [[ ! -z "$newPort" ]]; then
+    echo "changing port to $newPort"
+    pacmd set-source-port "$newSource" "$newPort"
+fi
